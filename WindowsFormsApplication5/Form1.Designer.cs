edbf2343fb1e50a71e592232f4582ae9e9dc2263
Kalana Edirisooriya <Kalana Edirisooriya@BOKUTTA>
2018-04-26 11:58:30 +0530

    Add project files.

---
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt__units = new System.Windows.Forms.TextBox();
+            this.cb_types = new System.Windows.Forms.ComboBox();
+            this.btn_cal = new System.Windows.Forms.Button();
+            this.btn_ext = new System.Windows.Forms.Button();
+            this.lbl_ba = new System.Windows.Forms.Label();
+            this.lbl_tax = new System.Windows.Forms.Label();
+            this.lbl_ta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Customer Type :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Consumed Units :";
+            // 
+            // txt__units
+            // 
+            this.txt__units.Location = new System.Drawing.Point(137, 96);
+            this.txt__units.Name = "txt__units";
+            this.txt__units.Size = new System.Drawing.Size(167, 20);
+            this.txt__units.TabIndex = 2;
+            // 
+            // cb_types
+            // 
+            this.cb_types.FormattingEnabled = true;
+            this.cb_types.Items.AddRange(new object[] {
+            "Domestic",
+            "Business"});
+            this.cb_types.Location = new System.Drawing.Point(137, 66);
+            this.cb_types.Name = "cb_types";
+            this.cb_types.Size = new System.Drawing.Size(167, 21);
+            this.cb_types.TabIndex = 3;
+            this.cb_types.Text = "Select Type";
+            // 
+            // btn_cal
+            // 
+            this.btn_cal.Location = new System.Drawing.Point(137, 134);
+            this.btn_cal.Name = "btn_cal";
+            this.btn_cal.Size = new System.Drawing.Size(124, 23);
+            this.btn_cal.TabIndex = 4;
+            this.btn_cal.Text = "Calculate";
+            this.btn_cal.UseVisualStyleBackColor = true;
+            this.btn_cal.Click += new System.EventHandler(this.btn_cal_Click);
+            // 
+            // btn_ext
+            // 
+            this.btn_ext.Location = new System.Drawing.Point(267, 133);
+            this.btn_ext.Name = "btn_ext";
+            this.btn_ext.Size = new System.Drawing.Size(36, 23);
+            this.btn_ext.TabIndex = 5;
+            this.btn_ext.Text = "Exit";
+            this.btn_ext.UseVisualStyleBackColor = true;
+            this.btn_ext.Click += new System.EventHandler(this.btn_ext_Click);
+            // 
+            // lbl_ba
+            // 
+            this.lbl_ba.AutoSize = true;
+            this.lbl_ba.Location = new System.Drawing.Point(137, 177);
+            this.lbl_ba.Name = "lbl_ba";
+            this.lbl_ba.Size = new System.Drawing.Size(10, 13);
+            this.lbl_ba.TabIndex = 6;
+            this.lbl_ba.Text = " ";
+            // 
+            // lbl_tax
+            // 
+            this.lbl_tax.AutoSize = true;
+            this.lbl_tax.Location = new System.Drawing.Point(137, 207);
+            this.lbl_tax.Name = "lbl_tax";
+            this.lbl_tax.Size = new System.Drawing.Size(10, 13);
+            this.lbl_tax.TabIndex = 7;
+            this.lbl_tax.Text = " ";
+            // 
+            // lbl_ta
+            // 
+            this.lbl_ta.AutoSize = true;
+            this.lbl_ta.Location = new System.Drawing.Point(137, 239);
+            this.lbl_ta.Name = "lbl_ta";
+            this.lbl_ta.Size = new System.Drawing.Size(10, 13);
+            this.lbl_ta.TabIndex = 8;
+            this.lbl_ta.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 273);
+            this.Controls.Add(this.lbl_ta);
+            this.Controls.Add(this.lbl_tax);
+            this.Controls.Add(this.lbl_ba);
+            this.Controls.Add(this.btn_ext);
+            this.Controls.Add(this.btn_cal);
+            this.Controls.Add(this.cb_types);
+            this.Controls.Add(this.txt__units);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Electricity Bill Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt__units;
+        private System.Windows.Forms.ComboBox cb_types;
+        private System.Windows.Forms.Button btn_cal;
+        private System.Windows.Forms.Button btn_ext;
+        private System.Windows.Forms.Label lbl_ba;
+        private System.Windows.Forms.Label lbl_tax;
+        private System.Windows.Forms.Label lbl_ta;
+    }
+}
+
---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cal_Click(object sender, EventArgs e)
+        {
+            {
+
+                double ba, uc, tax, ta;
+                uc = Convert.ToDouble(txt__units.Text);
+                                
+              
+                if (cb_types.SelectedIndex==0)
+                {
+                    if (uc > 100)
+                    {
+                        ba = uc * 30;
+                        tax = ba * .05;
+                    }
+
+                    else if (uc > 50)
+                    {
+                        ba = uc * 20;
+                        tax = ba * .03;
+                    }
+
+                    else
+                    {
+                        ba = uc * 10;
+                        tax = ba * .02;
+                    }
+
+                    ta = ba + tax;
+
+                    lbl_ba.Text = ba.ToString();
+                    lbl_tax.Text = tax.ToString();
+                    lbl_ta.Text = ta.ToString();
+                }
+
+                else if (cb_types.SelectedIndex == 1)
+                {
+                    if (uc > 100)
+                    { 
+                        ba = uc * 100;
+                        tax = ba * .1;
+                    }
+
+                    else if (uc > 50)
+                    {
+                        ba = uc * 50;
+                        tax = ba * .08;
+                    }
+
+                    else
+                    {
+                        ba = uc * 20;
+                        tax = ba * .05;
+                    }
+
+                    ta = ba + tax;
+
+                    lbl_ba.Text = "Bill Amount :" + ba.ToString();
+                    lbl_tax.Text = "Tax Amount :" + tax.ToString();
+                    lbl_ta.Text = "Total Bill Amount :" + ta.ToString();
+                }
+
+
+
+
+
+
+            }
+
+
+}
+
+        private void btn_ext_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
